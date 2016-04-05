67d6e14b891935861f7fd997102867a18c6e25a6
John Wang <john29917958@gmail.com>
2016-04-06 00:06:00 +0800

    Hellow World.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -17,8 +17,7 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  '/bower_components/bootstrap/dist/css/bootstrap.min.css',
-  'app/blah.css',
+  '/packages/bootstrap/dist/css/bootstrap.min.css',
   'styles/**/*.css'
 ];
 
@@ -30,9 +29,9 @@ var jsFilesToInject = [
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
-  '/bower_components/jquery/dist/jquery.min.js',
-  '/bower_components/react/react.min.js',
-  '/bower_components/react/react-dom.min',
+  '/packages/jquery/dist/jquery.min.js',
+  '/packages/react/react.min.js',
+  '/packages/react/react-dom.min',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
