d9dd7292ddff30ef02dedbb3ba157d2f576e8da9
John Wang <john29917958@gmail.com>
2016-04-06 00:52:03 +0800

    Creates Message API.

---
+++ api/controllers/MessageController.js
@@ -0,0 +1,29 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side logic for managing Messages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	find: function (req, res) {
+
+	},
+
+	findOne: function (req, res) {
+
+	},
+
+	create: function (req, res) {
+
+	},
+
+	update: function (req, res) {
+
+	},
+
+	destroy: function (req, res) {
+		
+	}
+};
+
